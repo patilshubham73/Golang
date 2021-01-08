2337b3391b0a3b6a90bbade882a53f123c6516f5
shubham patil <patilshubham73@gmail.com>
2021-01-08 11:02:07 +0530

    hello world and Keywords

---
+++ Identifiers and Keywords/Day_1.1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var pname="shubham"
+	var lname="patil"
+	var cname="golang"
+
+	fmt.Printf("name is :%s",pname)
+	fmt.Printf("\nLname is :%s",lname)
+	fmt.Printf("\ncname is :%s",cname)
+	
+
+}
