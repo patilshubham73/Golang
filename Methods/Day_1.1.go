16ed02632e2b1cc49b761e942a3090c9a0e70b51
shubham patil <patilshubham73@gmail.com>
2021-01-08 13:50:46 +0530

    Methods

---
+++ Methods/Day_1.1.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type author struct {
+	name string
+	branch string
+	particles int
+	salary int
+}
+
+func (a author)show(){
+	fmt.Println("Author Name :",a.name)
+	fmt.Println("Branch Name :",a.branch)
+	fmt.Println("Published articles :",a.particles)
+	fmt.Println("Salary ",a.salary)
+}
+
+func main(){
+	res:= author{
+		name:"sonu"
+		branch:"cse"
+		particles:203
+		salary:34000
+	}
+
+	res.show()
+}
